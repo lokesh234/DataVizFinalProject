dc64c576be455aeb4d6c48f1557b687a2dbfc42f
lokesh234 <lgangaramaney@wpi.edu>
2020-11-06 16:33:17 -0500

    Completed

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -56,7 +56,6 @@ function TopBar() {
                     <Button component={ Link } to="/heatmap" color="inherit" className={classes.menuButton}>Heatmap</Button>
                     <Button component={ Link } to="/piechart" color="inherit" className={classes.menuButton}>Pie Chart</Button>
                     <Button component={ Link } to="/barchartstate" color="inherit" className={classes.menuButton}>Bar Chart</Button>
-                    <Button component={ Link } to="/customizableviz" color="inherit" className={classes.menuButton}> Customizable Data Viz </Button>
                 </Toolbar>
             </AppBar>
         </Fragment>
